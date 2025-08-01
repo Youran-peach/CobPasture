f0815a2a8ef49b83bb5956d61570b9ca44245a8c
FullIdle <2675423979@qq.com>
2025-08-01 23:22:21 +0800

    更新红线道具的消耗(不变没有下次再说)

--- src/main/kotlin/org/figsq/cobpasture/cobpasture/api/breedlogic/genetic/IVGenetic.kt
+++ src/main/kotlin/org/figsq/cobpasture/cobpasture/api/breedlogic/genetic/IVGenetic.kt
@@ -1,8 +1,10 @@
 package org.figsq.cobpasture.cobpasture.api.breedlogic.genetic
 
+import com.cobblemon.mod.common.api.pokemon.stats.Stat
 import com.cobblemon.mod.common.api.pokemon.stats.Stats
 import com.cobblemon.mod.common.pokemon.Pokemon
 import com.cobblemon.mod.common.pokemon.helditem.CobblemonHeldItemManager
+import org.figsq.cobpasture.cobpasture.CobPasture
 import kotlin.random.Random
 
 /**
@@ -17,19 +19,52 @@ object IVGenetic : IGeneticHandler {
         parent2: Pokemon,
         egg: Pokemon
     ) {
-        if (!hasDestinyKnot(parent1, parent2)) return
-        val notGenetic = Stats.PERMANENT.random()
-        for (stat in Stats.PERMANENT) {
-            if (notGenetic == stat) continue
-            egg.setIV(stat, if (Random.nextBoolean()) parent1.ivs[stat]!! else parent2.ivs[stat]!!)
+        whoHasDestinyKnot(parent1, parent2)?.let {
+            val notGenetic = Stats.PERMANENT.random()
+            tryConsumeKnot(pokemon = it)
+            genetic5iv(parent1, parent2, egg, notGenetic)
         }
     }
 
+    fun tryConsumeKnot(pokemon: Pokemon){
+        if (!destinyknotConsume || !hasDestinyKnot(pokemon)) return
+        val copy = pokemon.heldItem().copy()
+        copy.count--
+        pokemon.swapHeldItem(copy,false)
+    }
 
-    fun hasDestinyKnot(parent1: Pokemon, parent2: Pokemon): Boolean {
-        val showdownId = CobblemonHeldItemManager.showdownId(parent1.heldItem())
-        println(showdownId)
-        return "destinyknot" == showdownId ||
-                "destinyknot" == CobblemonHeldItemManager.showdownId(parent2.heldItem())
+    /**
+     * 遗传5个个体从父母宝可梦随机，排除其中一个
+     * @param notGenetic 排除的一项
+     * @param parent1 父母宝可梦
+     * @param parent2 父母宝可梦
+     * @param target 目标宝可梦
+     */
+    fun genetic5iv(parent1: Pokemon, parent2: Pokemon, target: Pokemon, notGenetic: Stat) {
+        for (stat in Stats.PERMANENT) {
+            if (notGenetic == stat) continue
+            target.setIV(stat, if (Random.nextBoolean()) parent1.ivs[stat]!! else parent2.ivs[stat]!!)
+        }
+    }
+
+    /**
+     * 返回是哪知得红线生效了，多个也只会有一个
+     * @return 没有宝可梦拥有则返回空
+     */
+    fun whoHasDestinyKnot(parent1: Pokemon, parent2: Pokemon): Pokemon? {
+        return if (hasDestinyKnot(parent1)) parent1 else
+            if (hasDestinyKnot(parent2)) parent2 else null
+    }
+
+    fun hasDestinyKnot(pokemon: Pokemon): Boolean {
+        return "destinyknot" == CobblemonHeldItemManager.showdownId(pokemon.heldItem())
+    }
+
+    //默认消耗
+    var destinyknotConsume: Boolean = true
+
+    //加载配置所需项
+    fun load() {
+        destinyknotConsume = CobPasture.INSTANCE.config.getBoolean("destinyknot-consume")
     }
 }
